aba5c82789793701d4cc19ff711273a55c5caeee
Isael1601 <isael1601@gmail.com>
2022-06-16 00:25:11 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom/client';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Inicio from './components/Inicio/Inicio';
+import Menu from './components/Menú/Menu';
+import Nosotros from './components/Nosotros/Nosotros';
+import Delivery from './components/Delivery/Delibery';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Inicio />} />
+          <Route path='Inicio' element={<Inicio />} />
+          <Route path='Menu' element={<Menu />} />
+          <Route path='Nosotros' element={<Nosotros />} />
+          <Route path='Delibery' element={< Delivery />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Inicio/Inicio.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import style from "../style/style.scss";
+import colibri from "../assets/colibri.png"
+
+
+const Inicio = () => {
+
+    return(
+
+        <div>
+            <Navbar className="header">
+                <Container className="container">
+                    <Navbar.Brand id="brand" href="/">Taiyari "Hecho con el corazón"</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link className="a" as={Link} to="/Inicio">Inicio</Nav.Link>
+                        <Nav.Link className="a" as={Link} to="/Menu">Menú</Nav.Link>
+                        <Nav.Link className="a" as={Link} to="/Nosotros">Nosotros</Nav.Link>
+                        <Nav.Link className="a" as={Link} to="/Delibery">Delibery</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <section>
+                <div>
+                    
+                </div>
+            </section>
+
+            <section>
+                <div>
+                    <footer className="footer">
+                        <div className="footer-grid ">
+                            <div  id="grid-item-01 " >
+                                <h1 className="footer_h1 ">Taiyari</h1> 
+                                <p className="footer-p1">Comida Mexicana</p>
+                                <img className="colibri"
+                                    src={ colibri }
+                                />
+                            </div>
+                            <div id="grid-item-02 ">
+                                <label className="footer_label">Contacto</label>
+                                <p class="footerP ">Email: isael1601@gmail.com</p>
+                                <p class="footerP ">Cel. 5535225610</p>
+                            </div>
+                                
+                            <div className="grid-item-04 ">
+                                    <p><label class="footer_label ">Redes Sociales</label></p>
+                                    <a id="redes" href="https://github.com/UCamp-fullstack-Equipo07/BOOT-M2-SEM13-PROY3.git " class="fa fa-lg fa-github fa-3x "></a>
+                                    <a id="redes" href="https://github.com/UCamp-fullstack-Equipo07/BOOT-M2-SEM13-PROY3.git " class="fa fa-lg fa-twitter "></a>
+                                    <a id="redes" href="https://github.com/UCamp-fullstack-Equipo07/BOOT-M2-SEM13-PROY3.git " class="fa fa-lg fa-facebook "></a>
+                                    <a id="redes" href="https://github.com/UCamp-fullstack-Equipo07/BOOT-M2-SEM13-PROY3.git " class="fa fa-lg fa-instagram "></a>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </section>
+
+        </div>
+
+    )
+};
+
+export default Inicio;

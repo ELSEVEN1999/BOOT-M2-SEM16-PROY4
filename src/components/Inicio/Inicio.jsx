0da7a5bd9681974f10f8e524d70cd86975c27e3c
Isael1601 <isael1601@gmail.com>
2022-06-20 21:35:31 -0500

    buttons

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import style from "../style/style.scss";
 import colibri from "../assets/contenido/colibri.png";
@@ -9,6 +9,7 @@ import entregas from "../assets/contenido/entregas.png";
 import bienvenidos from "../assets/contenido/bienvenido.png";
 import menu from "../assets/contenido/Menu.png";
 import promo from "../assets/contenido/promocion.png";
+
 
 
 const Inicio = () => {
@@ -45,6 +46,7 @@ const Inicio = () => {
                                         <h1>Bienvenidos</h1>
                                             <p >Agradecemos tu preferencia, te invitamos a degustar nuestra comida tradiconal Mexicana, platillos hechos con amor, con recetas originales y productos de la mas alta calidad</p>
                                             <p>Conoce más sobre el proyecto Taiyari tradicional, y forma parte de nuestro equipo</p>
+                                                <Button className="btn" variant="utline-primaory" size="lg" as={ Link } to="/Nosotros">!Haz click¡</Button>
                                     </div>
                             </div>
                         </section>
@@ -54,10 +56,9 @@ const Inicio = () => {
                                 <div id="im-m">
                                     <img src={ menu } alt="menu" />
                                 </div>
-
                                 <div className="tx-m">
                                     <h1 className="text-m">Conoce nuestro menú</h1>
-
+                                    <Button className="btn_2" variant="utline-primaory" as={ Link } to="/Menu" >!Aqui¡</Button>
                                 </div>
                             </div>
                         </section>
@@ -69,6 +70,7 @@ const Inicio = () => {
                                 <div className="text-promo">
                                         <h1>Menú del dia</h1>
                                             <p>En la compra de un platillo llevate sopa, agua y postre del día por $80 más</p>
+                                            <Button className="btn_3" variant="utline-primaory" size="lg"  as= { Link } to="/Delibery">Promo</Button>
                                 </div>
                                 
                         </section>
@@ -81,6 +83,7 @@ const Inicio = () => {
                                 <div className="text-d">
                                     <h1 >¡Haz tu pedido!</h1>
                                         <p >Llena tu registro y realiza tu orden desde nuestra sección Delibery ¡Es muy fácil!</p>
+                                        <Button className="btn_4" variant="utline-primaory"  as= { Link } to="/Delibery">Delibery</Button>
                                 </div>
                         </section>
                     
